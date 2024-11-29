ccac5ef4164f019c03ab553c2443df31b0d5edd8
StepanEremkin <stepa.bike@gmail.com>
2024-11-29 12:58:49 +0300

    Lexer fixed

--- Lexing/Lexer.cs
+++ Lexing/Lexer.cs
@@ -64,6 +64,7 @@ internal static class Lexer
 
             if (char.IsSymbol(c) || char.IsPunctuation(c))
             {
+                endAcum();
                 readSymbol(ref i, c);
                 continue;
             }
@@ -80,6 +81,12 @@ internal static class Lexer
         {
             Location loc = new(path, startCol, startLine, line, col);
 
+            if (acum.Length == 0)
+            {
+                updateStart();
+                return;
+            }
+
             if (Enum.TryParse<Keywords>(acum, out var result))
             {
                 tokens.Add(new KeywordToken(result, loc));
@@ -92,6 +99,9 @@ internal static class Lexer
             {
                 tokens.Add(new NameToken(acum, loc));
             }
+
+            acum = "";
+            updateStart();
 
             void processDigit()
             {
@@ -299,6 +309,24 @@ internal static class Lexer
                     break;
                 case ';':
                     addSymbolToken(Symbols.Semicolon);
+                    break;
+                case '{':
+                    addSymbolToken(Symbols.CurlyOpen);
+                    break;
+                case '}':
+                    addSymbolToken(Symbols.CurlyClose);
+                    break;
+                case '(':
+                    addSymbolToken(Symbols.CircleOpen);
+                    break;
+                case ')':
+                    addSymbolToken(Symbols.CircleClose);
+                    break;
+                case '[':
+                    addSymbolToken(Symbols.SquareOpen);
+                    break;
+                case ']':
+                    addSymbolToken(Symbols.SquareClose);
                     break;
                 default:
                     addSymbolToken(Symbols.Unknown);
