2f164c15ccffd42e45c0fc5667eff909c84f3c3b
IExploitableMan <pythonblack5000@gmail.com>
2026-01-28 22:03:48 +0300

    Command args, i suppose?..

--- Program.cs
+++ Program.cs
@@ -7,17 +7,50 @@ global using Cml.Lexing;
 global using Cml.Errors;
 global using Cml.Types;
 using Cml;
+using System.CommandLine;
 
 // string path = @"test.cml";
 // Console.WriteLine(Path.Combine(Directory.GetCurrentDirectory(), path));
 
 // string build_folder = args.Length > 0 ? args[0] : "/tmp";
 
+RootCommand rootCommand = new("Cml™ (crappy modular language) compiler");
+
+var inputsArg = new Argument<string[]>("inputs")
+{
+    Description = "Input source files",
+    Arity = ArgumentArity.ZeroOrMore
+};
+rootCommand.Arguments.Add(inputsArg);
+
+Option<string> outputOption = new("--output", ["-o"])
+{
+    DefaultValueFactory = (_) => "a.out",
+    Description = "Output file path",
+};
+rootCommand.Options.Add(outputOption);
+
+Option<bool> printTokensOption = new("--print-tokens", ["-t"])
+{
+    Description = "Print tokens from lexer",
+};
+rootCommand.Options.Add(printTokensOption);
+
+ParseResult parseResult = rootCommand.Parse(args);
+if (parseResult.Invoke() != 0) Environment.Exit(1);
+
+var inputs = parseResult.GetValue(inputsArg);
+if (inputs == null || inputs.Length == 0) // uhhhh...
+{
+    Console.WriteLine("Error: No input files provided");
+    Environment.Exit(1);
+}
+
 CmlProject cmlp = new(
-    "cmltest",
+    parseResult.GetValue(outputOption)!,
     "./",
-    ["test.cml", "test2.cml"],
-    printTokens: false
+    inputs,
+    printTokens: parseResult.GetValue(printTokensOption)
 );
 
 ErrorReporter errorer = Compiler.Compile(cmlp);
@@ -27,4 +60,5 @@ foreach (var e in errorer)
 {
     Console.WriteLine(e);
 }
-
+if (errorer.Count != 0)
+    Environment.Exit(1);
